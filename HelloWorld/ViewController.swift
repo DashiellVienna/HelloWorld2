0b26a41e72346997496377f15bd5554f5a46c4ce
Andrei <101584932+DashiellVienna@users.noreply.github.com>
2024-10-23 16:02:00 +0200

    Greeting Button was added.

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
+    @IBOutlet var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        greetingLabel.isHidden.toggle()
         
     }
-
-
+    
+    @IBAction func greetingButtonDidTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+        
+//        if greetingLabel.isHidden {
+//            sender.setTitle("Show Greeting", for: .normal)
+//        } else {
+//            sender.setTitle("Hide Greeting", for: .normal)
+//        }
+    }
+    
 }
 
